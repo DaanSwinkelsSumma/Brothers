f3cee507b0c8ab4bfa081cd2186ceea23662980f
DaanSwinkels <ps181583@summacollege.nl>
2019-09-02 14:03:05 +0200

    Registreren en inloggen

--- BrothersLiberation/resources/views/welcome.blade.php
+++ BrothersLiberation/resources/views/welcome.blade.php
@@ -81,9 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Brothers of liberation
                 </div>
-
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
