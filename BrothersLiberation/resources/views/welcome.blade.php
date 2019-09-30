c9f3e074c066873e71fe102f9c1febcef295f40e
DaanSwinkels <ps181583@summacollege.nl>
2019-09-30 15:27:00 +0200

    Styling 30-9

--- BrothersLiberation/resources/views/welcome.blade.php
+++ BrothersLiberation/resources/views/welcome.blade.php
@@ -1,35 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="container-fluid ">
+    <div class="row bol opc"> 
+        <div class="col"></div>
+        <div class="col boltext">
+            <h1>Brothers of Liberations</h1>
+            <h4>We are a brotherhood from the Netherlands. 🇳🇱</h5>
+        </div>
+        <div class="col"></div>
+    </div>
+</div>
 <div class="container">
+    <br>
     <div class="row">
-        <div class="col">
-            <h2>Brothers of liberation</h2>
-
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <h5>We are a brotherhood from the Netherlands. 🇳🇱</h5>
-
+        <div class="col">LOGO</div>
+        <div class="col-8">
+                <h1>Over ons</h1>
+                <h5>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</h5>
+     
         </div>
     </div>
     <br>
-    <div class="row">
-        <div class="col">
-            <h4>Over ons</h4>
-            <h5>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</h5>
-        </div>
-    </div>
-    <br>
-    <div class="row imagesalign">
-        <div class="col">
-            <img src="{{ asset('img/Bol.jpg') }}" alt="BOL" height="300" width="400" class="img-fluid">
-        </div>
-        <div class="col">
-            <img src="{{ asset('img/Bol2.jpg') }}" alt="BOL" height="300" width="400" class="img-fluid">
-        </div>
-    </div>
 </div>
 @endsection
 
